73f95c051c1811c180482863514af0d9cf038b0e
tetrau <tetrau01@gmail.com>
2020-12-02 05:51:44 +0800

    Implement core concept FileObject, an abstraction over files

---
+++ src/main/scala/BackupV.scala
@@ -0,0 +1,43 @@
+import java.nio.ByteBuffer
+import java.nio.file.{Paths, Path}
+import java.util.Base64
+import scala.jdk.CollectionConverters._
+
+object FileObject {
+  private def deserialize(s: String): FileObject = {
+    val serializedBytes = Base64.getUrlDecoder.decode(s)
+    val lastModifiedTime = ByteBuffer.wrap(serializedBytes.take(8).reverse).getLong()
+    val filePathAsString = new String(serializedBytes.drop(8), "UTF-8")
+    val filePath = Paths.get(filePathAsString)
+    FileObject(filePath, lastModifiedTime)
+  }
+
+  def deserializeFromPath(path: Path): FileObject = {
+    deserialize(path.iterator().asScala.mkString(""))
+  }
+
+  private def relativePath(base: Path, absPath: Path): Path = {
+    require(base.isAbsolute && absPath.isAbsolute)
+    base.relativize(absPath)
+  }
+
+  def apply(base: Path, absPath: Path, lastModifiedTime: Long): FileObject = {
+    FileObject(relativePath(base, absPath), lastModifiedTime)
+  }
+}
+
+case class FileObject(filePath: Path, lastModifiedTime: Long) {
+  private def serialize(): String = {
+    val pathAsBytes: Array[Byte] = filePath.toString.getBytes("UTF-8")
+    val buffer = ByteBuffer.allocate(8 + pathAsBytes.length)
+    buffer.put(pathAsBytes.reverse)
+    buffer.putLong(lastModifiedTime)
+    Base64.getUrlEncoder.encodeToString(buffer.array.reverse)
+  }
+
+  def serializeToPath(): Path = {
+    val serializedResult = serialize()
+    Paths.get(serializedResult.take(2), serializedResult.drop(2).sliding(255, 255).toSeq: _*)
+  }
+}
+
